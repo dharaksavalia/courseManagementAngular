5d0ea4eaabfced9b5c9e25b72952ae5fdd76c27a
dharaksavalia <dharaksavalia@github.com>
2018-10-29 18:18:46 -0400

    started working on admin page and registeration form

---
+++ src/app/Component/adminpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adminpage',
+  templateUrl: '../Template/adminpage.component.html',
+  styleUrls: ['../Style/adminpage.component.css']
+})
+export class AdminpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AdminpageComponent } from './Component/adminpage.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { RegisterationFormComponent } from './Component/registeration-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminpageComponent,
+    RegisterationFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
