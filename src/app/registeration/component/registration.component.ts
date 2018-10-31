b57a728f61e23c69ca9bc779e3c44948b1f34f8e
dharaksavalia <dharak.savalia@gmail.com>
2018-10-31 13:43:06 -0400

    addition of registeration template

---
+++ src/app/registeration/component/registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: '../template/registration.component.html',
+  styleUrls: ['../style/registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/registeration/registeration.module.ts
+++ src/app/registeration/registeration.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RegistrationComponent } from './component/registration.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [RegistrationComponent],
+  exports: [RegistrationComponent]
 })
 export class RegisterationModule { }
