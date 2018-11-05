767a6785ca5ddea1d3a87ebed5a5bacf4b45af00
dharaksavalia <dharak.savalia@gmail.com>
2018-11-05 15:41:48 -0500

    updates to deploy on heroku

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 // server.js
 const express = require('express');
-const app = express();
 const path = require('path');
 
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/courseManagementFrontend/index.html'));
-});
-// Heroku port
+const app = express();
 
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8090);
 
